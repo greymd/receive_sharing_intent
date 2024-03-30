2c6ddc15719cad215f95836860e9cf5a92de342a
Yasuhiro Yamada <yamada@gr3.ie>
2024-03-30 20:40:46 +0000

    Update SwiftReceiveSharingIntentPlugin.swift

--- ios/Classes/SwiftReceiveSharingIntentPlugin.swift
+++ ios/Classes/SwiftReceiveSharingIntentPlugin.swift
@@ -188,7 +188,9 @@ public class SwiftReceiveSharingIntentPlugin: NSObject, FlutterPlugin, FlutterSt
     }
     
     private func decode(data: Data) -> [SharedMediaFile] {
-        let encodedData = try? JSONDecoder().decode([SharedMediaFile].self, from: data)
+        let str = String(decoding: data, as: UTF8.self).replacingOccurrences(of: ":2", with: ":\"file\"")
+        let fixedData = Data(str.utf8)
+        let encodedData = try? JSONDecoder().decode([SharedMediaFile].self, from: fixedData)
         return encodedData!
     }
     
